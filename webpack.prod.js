7aa4d835feb861429d30986791eb618c6f6e34e2
Nicole Ciemniak <nciemniak@gmail.com>
2019-11-03 18:25:14 -0600

    add missing require statement for terser

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,7 @@ const merge = require("webpack-merge");
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require('terser-webpack-plugin');
 
 const common = require("./webpack.common.js");
 
