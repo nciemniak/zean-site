2107441c632568b605bf98d5d630ae376ef30fac
Nicole Ciemniak <nciemniak@gmail.com>
2020-03-01 17:12:23 -0600

    fix modal image bugs

--- src/js/masonry-grid.js
+++ src/js/masonry-grid.js
@@ -6,25 +6,42 @@
  | Vendor page:  https://masonry.desandro.com/   |
  +-----------------------------------------------*/
 
+// SECTION: Set up event handlers here
 window.onload = function() {
-    var grid = document.querySelector('.grid');
-    var msnry = new Masonry(grid, {
-      itemSelector: '.grid-item',
-      columnWidth: '.grid-sizer',
-      percentPosition: true,
-    });
-    imagesLoaded(grid).on('progress', function () {
-      msnry.layout();
-    });
+  initMasonryGrid();
 }
 
+// open modal image when clicking on the grid image
 $('.grid-item').click(function() {
   var dataIndex = $(this).attr('data-index');
   $('#modal').show();
   $(`.modal-image[data-index="${dataIndex}"]`).show();
 });
 
+// close modal image when user presses 'x'
 $('.modal-icon').click(function() {
+  closeModalImage();
+});
+
+// close modal image when user presses esc key
+$(document).keyup(function(e) {
+  if (e.keyCode === 27) closeModalImage();   // esc
+});
+
+// SECTION: Define functions here
+function initMasonryGrid() {
+  var grid = document.querySelector('.grid');
+  var msnry = new Masonry(grid, {
+    itemSelector: '.grid-item',
+    columnWidth: '.grid-sizer',
+    percentPosition: true,
+  });
+  imagesLoaded(grid).on('progress', function () {
+    msnry.layout();
+  });
+}
+
+function closeModalImage() {
   $('#modal').hide();
   $('.modal-image').hide();
-});
+}
